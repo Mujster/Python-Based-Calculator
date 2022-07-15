6634e718eb6c1ac478ded516c880938dfc669430
Muhammad Mujtaba Ahmad <mmujtaba.ahmad@protonmail.com>
2022-07-16 01:26:35 +0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+print("\tWelcome To Python Based Calculator\n")
+num=input("\tEnter Your 1st Number\n")
+num2=input("\tnter Your 2nd Number\n")
+operand=input("\tEnter Your Operand(+,*,-,/,=,%):\n")
+if operand=='+':
+    print("The Addition of",num,"+",num2,":",int(num)+int(num2))
+elif operand=='*':
+    print("The Product of", num, "*", num2, ":", int(num) * int(num2))
+elif operand=='-':
+    print("The Subtraction of", num, "-", num2, ":", int(num) - int(num2))
+elif operand=='/':
+    print("The Division of", num, "/", num2, ":", int(num) / int(num2))
+elif operand=='%':
+    print("The Modulus of", num, "%", num2, ":", int(num) % int(num2))
+elif operand=='=':
+    if num==num2:
+        print("Both Numbers Are Equal\n")
+    else:
+        print("Both Numbers Are Not Equal\n")
+else:
+    print("No Such Operand Exists\n")
